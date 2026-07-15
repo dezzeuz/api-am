4b1a64ae5d3554a3cecc02dcc94050c409858d45
Dezzify <dezztobrut@gmail.com>
2026-07-15 20:34:41 +0700

    Create _middleware.js

---
+++ api/_middleware.js
@@ -0,0 +1,12 @@
+// Global CORS & OPTIONS handler
+export default function middleware(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+}
